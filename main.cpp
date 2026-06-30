573ef718a67c9b274321065408fe898718640863
tallalmohar <tallalmoohar@gmail.com>
2026-06-30 09:49:45 -0700

    added stringsToToken function: whitespace as delimeter by default

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+
+
+
+std::vector<std::string> stringToTokens(std::string& inputString){
+	std::vector<std::string> tokens;
+	std::istringstream str(inputString);
+	std::string token;
+	while(str >> token){
+		tokens.push_back(token);
+	}
+
+	return tokens;
+} 
+
 
 int main() {
 	bool continue_shell = true;
 	while(continue_shell){
+		// Basic UserInput 
 		std::string user_input;
 		std::cin >> user_input;
+		
+		//Shell Conditionals
 		if (user_input == "exit"){
 			continue_shell = false;
 		}
