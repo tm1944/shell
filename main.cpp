a63da6a99d6b833673a3e942a30b77419e924ecf
tallalmohar <tallalmoohar@gmail.com>
2026-06-30 11:59:31 -0700

    fixed bug: Prev we were passing individual strings in the line by themselves. this caused the print loop to prints multiple times. fix was using std::getline()

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
 
 
 
 
 // Input a string return vector<string> using whitespace as delimeter
-std::vector<std::string> stringToTokens(std::string& inputString){
+std::vector<std::string> stringToTokens(const std::string& inputString){
 	std::vector<std::string> tokens;
 	std::istringstream str(inputString);
 	std::string token;
@@ -23,20 +24,18 @@ int main() {
 	while(continue_shell){
 		// Basic UserInput 
 		std::string user_input;
-		std::cin >> user_input;
-		
+		std::getline(std::cin,user_input);
+
 		//Shell Conditionals
 		if (user_input == "exit"){
 			continue_shell = false;
 		}else//user inputted actual commands
-		{ 
-			for(std::string t : stringToTokens(user_input)){
+		{ 	
+			for(const std::string& t : stringToTokens(user_input)){
 				std::cout << "[" << t << "] ";
 			}
-			std::cout << "break out\n";
+			std::cout << std::endl;
 		}
-
-		std::cout << std::endl;
 	}
 	return 0;
 }
